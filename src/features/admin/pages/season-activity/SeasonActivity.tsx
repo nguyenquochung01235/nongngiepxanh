a715713676307bfd4d85b5326424b8281d4fdbb1
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-11-12 16:44:43 +0700

    resolve conflict

--- src/features/admin/pages/season-activity/SeasonActivity.tsx
+++ src/features/admin/pages/season-activity/SeasonActivity.tsx
@@ -94,11 +94,7 @@ const SeaSonActivity = () => {
   };
 
   const fetchCalendarList = () => {
-<<<<<<< HEAD
     return calendarApi.getAll({});
-=======
-    // return calendarApi.getAll();
->>>>>>> develop
   };
   const { isLoading, isError, data, error, isFetching } = useQuery(
     ["user/list"],
